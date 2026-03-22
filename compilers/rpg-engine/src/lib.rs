4afc416428a5d2799921c6ad84e7f7ef58933820
aster <192608617@qq.com>
2026-03-23 06:00:31 +0800

    mmm

--- compilers/rpg-engine/src/lib.rs
+++ compilers/rpg-engine/src/lib.rs
@@ -1,15 +1,21 @@
 #![warn(missing_docs)]
 
-#![warn(missing_docs)]
-
 pub mod input;
 pub mod color;
 pub mod tone;
 pub mod table;
 pub mod rect;
 pub mod types;
+pub mod sharedstate;
+pub mod graphics;
+pub mod audio;
+pub mod config;
 
 pub use color::Color;
 pub use tone::Tone;
 pub use rect::Rect;
 pub use table::Table;
+pub use sharedstate::{SharedState, RgssVersion, setup_shared_state, update_shared_state};
+pub use graphics::{Graphics, setup_graphics, update_graphics};
+pub use audio::{Audio, Bgm, Bgs, Me, Se, PlayState, setup_audio};
+pub use config::{Config, WindowMode, setup_config, load_config_from_json, save_config_to_json};
